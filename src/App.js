1d9eb36f5302e630a010df12130b144d8982fcf8
Stephen Done <srdone@gmail.com>
2017-01-11 22:37:15 -0700

    add a few definitions and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { definitions } from './definitions';
 
 class App extends Component {
   render() {
+    console.log(definitions);
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Economic Decision Making for Managers</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Definitions definitions={definitions}/>
       </div>
     );
   }
 }
+
+const Definitions = (props) => <ul className="definitions">{props.definitions.map(def => <li className="definition" key={def.name}>
+    <div className="definition__name">{def.name}</div> - <div className="definition__description">{def.description}</div>
+  </li>)}</ul>
 
 export default App;
---
+++ src/definitions.js
@@ -0,0 +1,22 @@
+export const definitions = [
+  {
+    name: 'Economic Good',
+    description: 'A material or service that provides satisfies human wants and provides utility'
+  },
+  {
+    name: 'Market Failure',
+    description: `A situation where the market equilibrium is not efficient.
+      That is, a condition exists where one person could be made better off
+      without making anyone else worse off.`
+  },
+  {
+    name: 'Monopoly',
+    description: 'When one supplier controls the entire market for a good.'
+  },
+  {
+    name: 'Natural Monopoly',
+    description: `Monopolies that arise naturally because it is more cost-effective for a single
+      producer to produce the entire output. This most often occurs when the producer experiences
+      diminishing marginal cost, often nearing zero, after a large up-front investment`
+  }
+];
