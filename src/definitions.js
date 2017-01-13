f099eeac13bf3736306dc5d29acba906281c6bb3
Stephen Done <srdone@gmail.com>
2017-01-12 21:21:16 -0700

    chapter 1 definitions

--- src/definitions.js
+++ src/definitions.js
@@ -1,13 +1,15 @@
 export const definitions = [
   {
-    name: 'Economic Good',
-    description: 'A material or service that provides satisfies human wants and provides utility'
+    name: 'Positive statements',
+    description: `What is. Statements based on facts and data.`
   },
   {
-    name: 'Market Failure',
-    description: `A situation where the market equilibrium is not efficient.
-      That is, a condition exists where one person could be made better off
-      without making anyone else worse off.`
+    name: `Normative statements`,
+    description: `What should be. Statements based on value judgements and opinions.`
+  },
+  {
+    name: 'Economic Good',
+    description: 'A material or service that provides satisfies human wants and provides utility'
   },
   {
     name: 'Monopoly',
@@ -18,5 +20,84 @@ export const definitions = [
     description: `Monopolies that arise naturally because it is more cost-effective for a single
       producer to produce the entire output. This most often occurs when the producer experiences
       diminishing marginal cost, often nearing zero, after a large up-front investment`
+  },
+  {
+    name: 'Pareto efficiency',
+    description: `A resource allocation where no one can be made better off without making someone
+      else worse off. This is the equilibrium that perfectly competitive markets attain.`
+  },
+  {
+    name: 'Kaldor-Hicks efficiency',
+    description: `Resource allocation that makes some better off and some worse off, but the net
+      benefits are positive. Theoretically, the gainers could fully compensate the losers so that
+      at least one person is better off and no person is worse off.`
+  },
+  {
+    name: 'Market Failure',
+    description: `A situation where the market equilibrium is not efficient.
+      That is, a condition exists where one person could be made better off
+      without making anyone else worse off.`
+  },
+  {
+    name: 'Externalities',
+    description: `A category of market failure. Externalities occur when actions of producers or
+      consumers impose additional costs on confer additional benefits upon others, without the others
+      receiving compensation or having to pay for the cost or benefit.`
+  },
+  {
+    name: 'Negative externality',
+    description: `An externality that imposes costs on others. Pollution is one example.`
+  },
+  {
+    name: 'Positive externality',
+    description: `An externality that creates benefits for others. An example would be a beekeeper
+      whose bees pollinate a neighbor's crops.`
+  },
+  {
+    name: 'Public goods',
+    description: `Goods that are non-rival and non-exclusive. A category of market failure. Does not
+      imply that the good is produced by the public sector. For example, radio is a non-rival, non-exclusive
+      good that is not produced exclusively by the public sector.`
+  },
+  {
+    name: 'Non-rival',
+    description: `Goods with the characteristic that one person's consumption of the good does not
+      diminish another person's ability to consume the good. An example of this is radio - If I tune in
+      to a radio station, it does not prevent anyone else from tuning in to the same station.`
+  },
+  {
+    name: 'Non-exclusive',
+    description: `Goods with the characteristic that it is practically impossible to exclude others
+      from consuming the good. An example is radio broadcasts.`
+  },
+  {
+    name: 'Imperfect information',
+    description: `Situations where producers or consumers do not have the information
+      they need to make efficient decisions.`
+  },
+  {
+    name: 'Adverse selection',
+    description: `A result of imperfect information where buyers and sellers have different (asymmetric) information.
+      Traders with information that others do not have selectively participate in the market. An example
+      of this is in the individual health insurance market where individuals have more information about
+      their own health than insurers do, leading those with low health risk to choose not to participate
+      in the market for health insurance as rates rise, which causes rates to rise further.`
+  },
+  {
+    name: 'Moral hazard',
+    description: `When one person takes additional risks because someone other than themself
+      bears the costs of those risks. An example would be someone who is less careful with their
+      phone when they have accident replacement insurance.`
+  },
+  {
+    name: 'Principal agent problem',
+    description: `When one person is able to make decisions for another person, and those
+      people have different incentives. An example would be a real-estate agent who may be more
+      concerned about making a quick sale than getting the highest possible value for a home.`
+  },
+  {
+    name: 'Distributional Equity',
+    description: `Normative questions about the distribution of benefits and costs among
+      individuals and groups in society.`
   }
 ];
