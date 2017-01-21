69d27d6f3fece4e2ab9a25f7e90451a5c1bce49f
Stephen Done <srdone@gmail.com>
2017-01-20 22:15:37 -0700

    testing - hopefully this fixes broken site

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Router, Route, browserHistory } from 'react-router';
+import { useBasename } from 'history';
 
 import App from './App';
 import { Communication, Economics } from './containers';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={useBasename(() => browserHistory)({basename: '/MPA612'})}>
     <Route path="/" component={App}>
       <Route path="economics" component={Economics}/>
       <Route path="communication" component={Communication}/>
