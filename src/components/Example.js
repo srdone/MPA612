1dd462266bd4c1ade74e32613d970855d0ea74ce
Stephen Done <srdone@gmail.com>
2017-01-21 21:34:55 -0700

    show examples

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-export const Example = props => <div className="example">Incorrect: {props.incorrect}, Correct: {props.correct}</div>;
+export const Example = props => <div className="example">
+  {props.example &&
+    <span>Example: {props.example}</span>
+  }
+  {!props.example &&
+    <span>Incorrect: {props.incorrect}, Correct: {props.correct}</span>
+  }
+ </div>;
 
 Example.defaultProps = {
   incorrect: 'No example',
